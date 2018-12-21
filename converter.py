79d3e1abf4d8069f909ee6c7d2fa4e09aae80342
DRMALEK <malekozel97@gmail.com>
2018-12-21 10:15:53 +0300

    Add files via upload

--- converter.py
+++ converter.py
@@ -1,10 +1,21 @@
 import textract
 import encodings
+import os
 
 def covert_to_text(file_path):
-    
-    text = textract.process(file_path)
+    try:
+        file_format = os.path.relpath(file_path).split(".")[1]
+    except:
+        return ("Please Try another file please!")
 
-    text = text.decode("utf-8")
+    supported_formats = ["pdf", "txt", "equb"]
 
-    return text
+    if file_format not in supported_formats:
+        return ("Try different file formats please!")
+
+    try:
+        text = textract.process(file_path)
+        text = text.decode("utf-8")
+        return text
+    except:
+        return ("Please Try another file please!")
--- data_fixor.py
+++ data_fixor.py
@@ -1,12 +1,53 @@
 import pandas  as pd
 import matplotlib.pyplot as plt
 import numpy as np
+from helper_functions import open_pickle
 
-from imblearn.over_sampling import ADASYN
-from imblearn.over_sampling import SMOTENC
+from imblearn.over_sampling import ADASYN   
 
 from imblearn.under_sampling import TomekLinks
-from imblearn.under_sampling import RandomUnderSample
 
 from imblearn.combine import SMOTETomek
+
+data = open_pickle("./pandas_data_frame.pkl")
+data_frame = pd.DataFrame(data=data)
+
+x = data_frame["movie_words"].tolist()
+
+y = data_frame["rating"].tolist()
+
+
+#make all the movies with same length of words, im not sure if the problem is from it or not
+
+max_words = 0
+
+for words in x: 
+    if len(words) > max_words: 
+        max_words = len(words)
+
+all_words_list = []
+
+for words in x:
+    while len(words) < max_words:
+        words.append(" ")
+
+    all_words_list.append(words)
+
+data["movie_words"] = all_words_list.copy()
+
+#Make a matrix with ( number_of_movies * max_words ) size
+
+x = data_frame["movie_words"].tolist()
+
+all_words = []
+
+for words in x:
+    all_words.extend(words)
+
+
+all_words = np.array(all_words)
+all_words = all_words.reshape((len(x), max_words))
+
+x_resample, y_resample = ada.fit_resample(all_words, y.ravel())
+
 
