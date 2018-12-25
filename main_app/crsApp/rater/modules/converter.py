64c9d232d603e146b104b83b890ab9cb84f0a1e5
DR.MAELK <malekozel97@gmail.com>
2018-12-25 20:53:41 +0300

    add files to backend

--- main_app/crsApp/rater/modules/converter.py
+++ main_app/crsApp/rater/modules/converter.py
@@ -3,19 +3,32 @@ import encodings
 import os
 
 def convert_to_text(file_path):
+    """
+        Error codes:
+            1- "01" > file corraped or with no format
+            2- "10" > file has format rather than pdf, txt or equb
+            3- "11" > Error while extracting text from the 
+            4- "00" > Error while opening the file
+    """
     try:
         file_name = file_path.split("/")[2]
         file_format = file_name.split(".")[1]
     except:
-        return ("Please Try another file please!")
+        return "01"
 
     supported_formats = ["pdf", "txt", "equb", "PDF", "TXT", "EQUB"]
 
     if file_format not in supported_formats:
-        return ("Try different file formats please!")
+        return "10"
+    
     try:
-        text = textract.process(os.getcwd() + file_path)
-        text = text.decode("utf-8")
-        return text
+        #try to open the file
+        input_file = open(os.getcwd() + file_path, "r")
+        try:
+            text = textract.process(os.getcwd() + file_path)
+            text = text.decode("utf-8")
+            return text
+        except:
+            return "11"
     except:
-        return ("Please Try another file please!")
+        return "00"
--- main_app/crsApp/rater/views.py
+++ main_app/crsApp/rater/views.py
@@ -10,7 +10,15 @@ import os
 
 # Create your views here.
 class Rater(TemplateView):
-
+    """
+    The rater class, responsible for rating the uploaded books
+    Exceptions:
+        1- The uploaded books is not in english words
+        2- The uploaded books is not in right format
+        3- The uploaded file is corrupted or no text can be extracted from it
+        4- 
+    
+    """
     def post(self, request):
         if request.FILES["uploaded_book"]:
             #Save the file to the raw file dir under "basedir + /media"
@@ -19,14 +27,40 @@ class Rater(TemplateView):
             input_file_name = input_file.name
             fs = FileSystemStorage()
             filename = fs.save(input_file.name, input_file)
-            uploaded_file_url = fs.url(filename)
+            uploaded_file_url = fs.url(filename)            
             #Convert the file to text, if it contain non english words or nothing or in not supported format
             text_file = converter.convert_to_text(uploaded_file_url)
-            #First step check if the book is already stored in the database, if so return the rating input_book
+            if text_file == "00":
+                response = {
+                    "message" : "Error while opening the book, make sure that the book exist and its name does not contain spaces!"
+                }
+                return render(request, "./rater/error.html", context=response)
+            if text_file == "01":
+                response = {
+                    "message" : "Book is  corraped or with no format, please try another book!"
+                }
+                return render(request, "./rater/error.html", context=response)
+            elif text_file == "10":
+                response = {
+                    "message" : "Book has a format rather than pdf, txt or equb, please try another book!"
+                }
+                return render(request, "./rater/error.html", context=response)
+            elif text_file == "11":
+                response = {
+                    "message" : "Error while extracting text from the book, please try another book!"
+                }
+                return render(request, "./rater/error.html", context=response)
             
-            #Second step convert the file, if it is in the correct format, if not return error
+            else:
+                #First step check if the book is already stored in the database, if so return the rating input_book
+                
+                #Second step convert the file, if it is in the correct format, if not return error
 
-            #Third preprocess the file and get its token and send to the model
+                #Third preprocess the file and get its token and send to the model
 
-            #Fourth, get the rating and return it to the user
-            return render_to_response("./rater/index.html", context=None, content_type=RequestContext(request))
+                #Fourth, get the rating and return it to the user
+                response = {
+                    "message" : "The rating of the book is ",
+                    "rating"  : "appropriate for children"
+                }
+                return render(request, "./rater/success.html", context=response)
